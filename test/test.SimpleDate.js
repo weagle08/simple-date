6a0aab432d316e5daeb7628f2c40ea3b522b2651
weagle08 <weagle08@gmail.com>
2015-07-22 23:14:33 -0500

    initial commit, finish unit test

---
+++ test/test.SimpleDate.js
@@ -0,0 +1,199 @@
+/**
+ * Created by Ben on 7/18/2015.
+ */
+var chai = require('./chai');
+var SimpleDate = require('../lib/SimpleDate');
+
+describe('SIMPLE DATE TEST ->', function(){
+    it('constructor empty', function(){
+        var normDate = new Date();
+        var date = new SimpleDate();
+
+        assert(date instanceof  SimpleDate);
+        assert(date.getYear() == normDate.getFullYear());
+        assert(date.getMonth() == normDate.getMonth());
+        assert(date.getDate() == normDate.getDate());
+    });
+
+    it('constructor Date', function() {
+        var normDate = new Date();
+        normDate.setDate(3);
+        normDate.setMonth(4);
+        normDate.setYear(2014);
+
+        var date = new SimpleDate(normDate);
+        assert(date.getYear() == 2014);
+        assert(date.getMonth() == 4);
+        assert(date.getDate() == 3);
+    });
+
+    it('constructor object', function(){
+        var date = new SimpleDate({year: 1964, month: 8, date: 18});
+        assert(date.getYear() == 1964);
+        assert(date.getMonth() == 8);
+        assert(date.getDate() == 18);
+    });
+
+    it('constructor SimpleDate', function(){
+        var test = new SimpleDate({year: 2001, month: 1, date: 9});
+        var date = new SimpleDate(test);
+        assert(date.getYear() == 2001);
+        assert(date.getMonth() == 1);
+        assert(date.getDate() == 9);
+    });
+
+    it('constructor values', function(){
+        var date = new SimpleDate(2013, 10, 15);
+        assert(date.getYear() == 2013);
+        assert(date.getMonth() == 10);
+        assert(date.getDate() == 15);
+    });
+
+    it('set days, months, years', function(){
+        var date = new SimpleDate();
+        date.setYear(2000);
+        date.setMonth(0);
+        date.setDate(31);
+
+        assert(date.getYear() == 2000);
+        assert(date.getMonth() == 0);
+        assert(date.getDate() == 31);
+
+    });
+
+    it('leap year', function(){
+        var date = new SimpleDate(2016, 1, 29);
+        assert(date.getDate() == 29);
+    });
+
+    it('construct invalid date', function(){
+        var date = new SimpleDate();
+        date.setYear(2000);
+        date.setMonth(0);
+        date.setDate(35);
+
+        assert(date.getYear() == 2000);
+        assert(date.getMonth() == 0);
+        assert(date.getDate() == 31);
+    });
+
+    it('add days', function() {
+
+        //add 10 days
+        var date = new SimpleDate(2014, 11, 25);
+        date.addDays(10);
+
+        assert(date.getYear() == 2015);
+        assert(date.getMonth() == 0);
+        assert(date.getDate() == 4);
+
+        date = new SimpleDate(2014, 10, 10);
+        date.addDays(10);
+
+        assert(date.getYear() == 2014);
+        assert(date.getMonth() == 10);
+        assert(date.getDate() == 20);
+    });
+
+    it('subtract days', function(){
+        //subtract 10 days
+        var date = new SimpleDate(2014, 0, 4);
+        date.addDays(-10);
+
+        assert(date.getYear() == 2013);
+        assert(date.getMonth() == 11);
+        assert(date.getDate() == 25);
+    });
+
+    it('add 365 days', function(){
+        var date = new SimpleDate(2015, 6, 19);
+        date.addDays(365);
+
+        assert(date.getYear() == 2016);
+        assert(date.getMonth() == 6);
+        //one less because of a leap year
+        assert(date.getDate() == 18);
+    });
+
+    it('subtract 365 days', function(){
+        var date = new SimpleDate(2016, 6, 18);
+        date.addDays(-365);
+
+        assert(date.getYear() == 2015);
+        assert(date.getMonth() == 6);
+        //one more because of a leap year
+        assert(date.getDate() == 19);
+    });
+
+    it('is leap year', function(){
+        var date = new SimpleDate(2016, 1, 1);
+        assert(date.isLeapYear() == true);
+    });
+
+    it('move to next ...', function(){
+        var date = new SimpleDate(2015, 6, 19);
+        date.moveToDayOfWeek(5);
+
+        assert(date.getYear() == 2015);
+        assert(date.getMonth() == 6);
+        assert(date.getDate() == 24);
+
+        //test moving across week
+        date = new SimpleDate(2015, 6, 25);
+        date.moveToDayOfWeek(3);
+
+        assert(date.getYear() == 2015);
+        assert(date.getMonth() == 6);
+        assert(date.getDate() == 29);
+
+        //test moving to next of same day
+        date = new SimpleDate(2015, 6, 25);
+        date.moveToDayOfWeek(6);
+
+        assert(date.getYear() == 2015);
+        assert(date.getMonth() == 7);
+        assert(date.getDate() == 1);
+    });
+
+    it('move to previous ...', function(){
+
+    });
+
+    it('add months', function(){
+
+    });
+
+    it('subtract months', function(){
+
+    });
+
+    it('add 12 months', function(){
+
+    });
+
+    it('subtract 12 months', function(){
+
+    });
+
+    it('serialize', function(){
+
+    });
+
+    it('toString', function(){
+
+    });
+
+    it('get days difference', function(){
+
+    });
+
+    it('get days difference LARGE', function(){
+
+    });
+
+    it('get days difference NONE', function(){
+
+    });
+
+
+});
